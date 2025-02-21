9823c9907e18e3392a8942dfac13f55285f2ec71
Anatoly <yatskovanatoly@gmail.com>
2025-02-21 07:55:21 +0300

    Random video blend-mode fxs, idk

--- src/app/komnata/page.tsx
+++ src/app/komnata/page.tsx
@@ -1,27 +1,43 @@
 'use client'
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 const Komnata = () => {
   const ref = useRef<HTMLVideoElement>(null)
+  const [randomFx, setRandomFx] = useState('')
 
   useEffect(() => {
     const video = ref.current
     if (video) {
+      setRandomFx(getRandomFx())
       video.playbackRate = 0.5
     }
   }, [])
 
   return (
-    <video
-      ref={ref}
-      src="/eta_komnata_vid_1.mp4"
-      className="object-cover h-full w-full"
-      loop
-      autoPlay
-      playsInline
-      muted
-    />
+    <>
+      <div className="bg-green-300 h-wull w-full inset-0 fixed" />
+      <video
+        ref={ref}
+        src="/eta_komnata_vid_1.mp4"
+        className={`object-cover h-full w-full ${randomFx}`}
+        onClick={() => setRandomFx(getRandomFx())}
+        loop
+        autoPlay
+        playsInline
+        muted
+      />
+    </>
   )
 }
+
+const fxs = [
+  'mix-blend-exclusion',
+  'mix-blend-multiply',
+  'mix-blend-darken',
+  'mix-blend-hard-light',
+  'mix-blend-overlay',
+]
+
+const getRandomFx = () => fxs[Math.floor(Math.random() * fxs.length)]
 
 export default Komnata
