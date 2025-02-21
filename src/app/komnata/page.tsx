c5eae768d60f7d0e3082ed16829bbef7b616d4a8
Anatoly <yatskovanatoly@gmail.com>
2025-02-21 08:08:02 +0300

    Some minor tweaks

--- src/app/komnata/page.tsx
+++ src/app/komnata/page.tsx
@@ -20,7 +20,8 @@ const Komnata = () => {
         ref={ref}
         src="/eta_komnata_vid_1.mp4"
         className={`object-cover h-full w-full ${randomFx}`}
-        onClick={() => setRandomFx(getRandomFx())}
+        onClick={() => setRandomFx(getRandomFx(randomFx))}
+        poster="/2025-02-13 19.06.42.jpg"
         loop
         autoPlay
         playsInline
@@ -38,6 +39,9 @@ const fxs = [
   'mix-blend-overlay',
 ]
 
-const getRandomFx = () => fxs[Math.floor(Math.random() * fxs.length)]
+const getRandomFx = (currentFx?: string) =>
+  fxs.filter((fx) => fx !== currentFx)[
+    Math.floor(Math.random() * (currentFx ? fxs.length - 1 : fxs.length))
+  ]
 
 export default Komnata
