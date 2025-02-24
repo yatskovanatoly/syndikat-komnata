e338ea6ac1707947e782b46005fc8cb54c92898d
Anatoly <yatskovanatoly@gmail.com>
2025-02-24 03:11:40 +0300

    Test subdomain on prod

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'komnata.syndikat.moscow',
+          },
+        ],
+        destination: '/komnata/:path*',
+      },
+    ]
+  },
 }
 
 export default nextConfig
