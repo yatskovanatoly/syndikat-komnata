a578d9a761cfb3d6ae85d944314cc68cf258bb75
Anatoly <yatskovanatoly@gmail.com>
2025-02-24 16:08:10 +0300

    Init

--- next.config.ts
+++ next.config.ts
@@ -1,44 +1,5 @@
-import { DOMAIN } from '@/middleware'
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/komnata/:path*',
-        destination: getDestination('komnata'),
-        permanent: true,
-      },
-    ]
-  },
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: origin },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, RSC, Next-Router-Prefetch, Next-Router-State-Tree',
-          },
-          {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on',
-          },
-        ],
-      },
-    ]
-  },
-}
-
-const protocol = process.env.NODE_ENV === 'production' ? 'https://' : 'http://'
-const getDestination = (path: string) => `${protocol}${path}.${DOMAIN}`
-const origin = `${protocol}${DOMAIN}`
+const nextConfig: NextConfig = {}
 
 export default nextConfig
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,44 @@
-import Link from 'next/link'
+'use client'
 
-export default function Home() {
+import { useEffect, useRef, useState } from 'react'
+
+const Komnata = () => {
+  const ref = useRef<HTMLVideoElement>(null)
+  const [randomFx, setRandomFx] = useState<string | undefined>(undefined)
+
+  useEffect(() => {
+    const video = ref.current
+    if (video) {
+      video.playbackRate = 0.5
+    }
+  }, [])
+
   return (
-    <div className="p-2">
-      <span className="text-9xl font-bold text-wrap break-words select-none">
-        МОСКОВСКИЙМУЗЫКАЛЬНЫЙСИНДИКАТ
-      </span>
-      <Link href={'/komnata'} className="text-4xl underline text-nowrap">
-        эта комната
-      </Link>
-    </div>
+    <>
+      {randomFx && (
+        <div className="bg-green-300 h-full w-full inset-0 absolute" />
+      )}
+      <video
+        ref={ref}
+        src="/eta_komnata_vid_1.mp4"
+        className={`object-cover h-full w-full ${randomFx}`}
+        onClick={() => setRandomFx(getRandomFx(randomFx))}
+        preload="metadata"
+        loop
+        autoPlay
+        playsInline
+        muted
+        onLoadedData={() => setRandomFx(getRandomFx(randomFx))}
+      />
+    </>
   )
 }
+
+const fxs = ['mix-blend-multiply', 'mix-blend-darken', 'mix-blend-hard-light']
+
+const getRandomFx = (currentFx?: string) =>
+  fxs.filter((fx) => fx !== currentFx)[
+    Math.floor(Math.random() * (currentFx ? fxs.length - 1 : fxs.length))
+  ]
+
+export default Komnata
