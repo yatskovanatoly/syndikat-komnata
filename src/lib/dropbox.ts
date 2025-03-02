b3297aa1a9e9dd96a62d4c28e1fa35651bb6efb9
Anatoly <yatskovanatoly@gmail.com>
2025-03-02 15:28:56 +0300

    Fix env back :)

--- src/lib/dropbox.ts
+++ src/lib/dropbox.ts
@@ -1,8 +1,6 @@
 import { Dropbox } from 'dropbox'
 
-const DROPBOX_ACCESS_TOKEN =
-  process.env.NEXT_PUBLIC_DROPBOX_ACCESS_TOKEN ||
-  process.env.DROPBOX_ACCESS_TOKEN
+const DROPBOX_ACCESS_TOKEN = process.env.NEXT_PUBLIC_DROPBOX_ACCESS_TOKEN
 
 export async function getDropboxFiles() {
   if (!DROPBOX_ACCESS_TOKEN) {
