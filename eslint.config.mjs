b6e531ade548c51d48e5ff6b613f552765c4def3
Anatoly <yatskovanatoly@gmail.com>
2025-03-02 14:44:00 +0300

    No implicit any - false, @next/env

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      'no-implicit-any': false,
+    },
+  },
 ]
 
 export default eslintConfig
