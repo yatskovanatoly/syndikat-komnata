c7e27cac09ef993d1b3dc20fc3b5208082aa56f1
Anatoly <yatskovanatoly@gmail.com>
2025-03-10 22:02:27 +0300

    Feat OpenGraph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,13 +38,27 @@ const geistMono = Geist_Mono({
 })
 
 export const metadata: Metadata = {
-  title: 'московский музыкальный синдикат',
+  title: 'эта комната • московский музыкальный синдикат',
+  description: 'спектакль-сказка о поиске дома',
   openGraph: {
-    type: 'website',
     title: 'эта комната • московский музыкальный синдикат',
-    siteName: 'московский музыкальный синдикат',
-    // images: [{
-    //   url: "https://example.com/og.png",
-    // }],
+    description: 'спектакль-сказка о поиске дома',
+    url: 'https://komnata.syndikat.moscow',
+    type: 'website',
+    images: [
+      {
+        url: 'https://opengraph.b-cdn.net/production/images/46fb5671-9c66-4a61-b9b9-4c17e74683a4.png?token=SulmU6NZKEj92F7uB6JirqB5O3C84_NQw45veuJSCvQ&height=630&width=1200&expires=33277632940',
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'эта комната • московский музыкальный синдикат',
+    description: 'спектакль-сказка о поиске дома',
+    images: [
+      'https://opengraph.b-cdn.net/production/images/46fb5671-9c66-4a61-b9b9-4c17e74683a4.png?token=SulmU6NZKEj92F7uB6JirqB5O3C84_NQw45veuJSCvQ&height=630&width=1200&expires=33277632940',
+    ],
   },
 }
