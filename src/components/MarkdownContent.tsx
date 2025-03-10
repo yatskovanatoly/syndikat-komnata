0913acbd012dd73cc602666505ef9cc87791f7ce
Anatoly <yatskovanatoly@gmail.com>
2025-03-10 22:35:29 +0300

    Minor headings styling

--- src/components/MarkdownContent.tsx
+++ src/components/MarkdownContent.tsx
@@ -3,24 +3,17 @@ import Markdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
 
 const MarkdownContent = async () => {
-  const description = await getDescription(
-    process.env.NEXT_PUBLIC_DESCRIPTION || ''
-  )
+  const description = await getDescription(url)
+  const commonStyles = 'font-bold font-serif mb-[calc(var(--spacing)*4)]'
   return (
     <Markdown
       components={{
         p: (props) => (
           <p className="font-mono whitespace-pre-wrap leading-7" {...props} />
         ),
-        h1: (props) => (
-          <h1 className="text-4xl font-bold font-serif" {...props} />
-        ),
-        h2: (props) => (
-          <h2 className="text-2xl font-bold font-serif" {...props} />
-        ),
-        h3: (props) => (
-          <h3 className="text-xl font-bold font-serif" {...props} />
-        ),
+        h1: (props) => <h1 className={`text-4xl ${commonStyles}`} {...props} />,
+        h2: (props) => <h2 className={`text-2xl ${commonStyles}`} {...props} />,
+        h3: (props) => <h3 className={`text-xl ${commonStyles}`} {...props} />,
       }}
       remarkPlugins={[remarkGfm]}
     >
@@ -32,17 +25,16 @@ const MarkdownContent = async () => {
 const getDescription = async (url: string) => {
   const t = await getTranslations()
   const res = await fetch(url)
-    .then((res) => {
-      if (!res.ok) {
-        throw new Error(`Failed to fetch: ${res.status} ${res.statusText}`)
-      }
-      return res.text()
-    })
+    .then((res) => res.text())
     .catch((error) => {
       console.error('Markdown fetch error:', error)
     })
   if (!res) return t('Description.error')
   return res
 }
+
+const url = `https://${process.env.NEXT_PUBLIC_DESCRIPTION}`
+
+console.log(url)
 
 export default MarkdownContent
