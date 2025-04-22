9495bfffbd53fa8e0f59a5575313fe05bdc0c03d
Anatoly <yatskovanatoly@gmail.com>
2025-04-22 15:35:14 +0300

    Rm scroll snap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,14 +9,14 @@ const Page = async () => {
   return (
     <div className="flex flex-col h-dvh">
       <Hero />
-      <main className="flex-1 overflow-y-auto sm:snap-y sm:snap-mandatory px-4">
-        <section className="h-dvh flex flex-col justify-end  snap-end max-w-screen-sm w-full py-4 ">
-          <h1 className="text-7xl sm:text-9xl border-4 relative px-2 snap-start snap-always scroll-mt-4">
+      <main className="flex-1 overflow-y-auto px-4">
+        <section className="h-dvh flex flex-col justify-end max-w-screen-sm w-full py-4 ">
+          <h1 className="text-7xl sm:text-9xl border-4 relative px-2 scroll-mt-4">
             {t('title')}
             {/* <div className='absolute right-4 top-4 text-2xl sm:text-4xl'>{t('date')}</div> */}
           </h1>
         </section>
-        <section className="min-h-dvh snap-start snap-always scroll-mt-4 flex">
+        <section className="min-h-dvh scroll-mt-4 flex">
           <div className="grow flex flex-wrap justify-start h-fit items-center w-full gap-4">
             <div className="w-full max-w-screen-sm self-start">
               <MarkdownViewer />
@@ -27,7 +27,7 @@ const Page = async () => {
             </div>
           </div>
         </section>
-        <section className="py-2 snap-start">
+        <section className="py-2">
           <Footer />
         </section>
       </main>
